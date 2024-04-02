b2ca8fca0cbfcc624e68c790ade40ec5af5e8b0b
William Kearney <william.kearney@uni-potsdam.de>
2024-04-02 12:15:18 +0200

    Add export definition macros
    Windows requires extra attributes to export the symbols from a shared
    library. Macros similar to the ones defined here are a typical way of
    handling such attributes.
    
    include/topotoolbox.h adds the TOPOTOOLBOX_API macro, which controls
    which __declspec attribute is attached to exported API functions. The
    TOPOTOOLBOX_BUILD flag controls whether the shared library is being
    built, in which case the symbols must be exported, or used, in which
    case they must be imported. An API function `has_topotoolbox` is
    added. This function returns 1 (true), and can be used to test that
    everything is linked correctly. As more functions are added to the
    library, this functionality may become less useful.
    
    src/topotoolbox.c defines the TOPOTOOLBOX_BUILD macro and the
    `has_topotoolbox` function.
    
    test/versioninfo.cpp now uses the `has_topotoolbox` function to check
    before printing the version number.

--- include/topotoolbox.h
+++ include/topotoolbox.h
@@ -1,8 +1,31 @@
 #ifndef TOPOTOOLBOX_H
 #define TOPOTOOLBOX_H
 
+#if defined(TOPOTOOLBOX_BUILD)
+#if defined(_WIN32)
+#define TOPOTOOLBOX_API __declspec(dllexport)
+#else
+#define TOPOTOOLBOX_API
+#endif
+#else
+#if defined(_WIN32)
+#define TOPOTOOLBOX_API __declspec(dllimport)
+#else
+#define TOPOTOOLBOX_API
+#endif
+#endif
+
 #define TOPOTOOLBOX_VERSION_MAJOR 3
 #define TOPOTOOLBOX_VERSION_MINOR 0
 #define TOPOTOOLBOX_VERSION_PATCH 0
+
+/*
+  has_topotoolbox()
+
+  Returns 1. Used to ensure that topotoolbox is compiled and linked
+  correctly.
+ */
+TOPOTOOLBOX_API
+int has_topotoolbox(void);
 
 #endif
