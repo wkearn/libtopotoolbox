b8ab64929829f45a1fe1433274773c22bfa4fb10
William Kearney <william.kearney@uni-potsdam.de>
2024-05-15 13:58:38 +0200

    Add logo to docs

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,8 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_logo = 'logo.png'
+
 # Breathe configuration
 breathe_default_project = "TopoToolbox"
 
