ad3d9f2cc04d4c69ae5d63ecb330f30dd67019d1
William Kearney <william.kearney@uni-potsdam.de>
2024-05-31 12:40:09 +0200

    Test fast sweeping method
    This extracts the gradient computation to a function, so it is more
    easily reused.
    
    This also corrects an error in the slope limiting tests

--- test/excesstopography.cpp
+++ test/excesstopography.cpp
@@ -31,9 +31,32 @@ static float pcg4d(uint32_t a, uint32_t b, uint32_t c, uint32_t d) {
   return (float)(w >> 8) / (1 << 24);
 }
 
+float upwind_gradient(float *u, ptrdiff_t row, ptrdiff_t col, float cellsize,
+                      ptrdiff_t nrows, ptrdiff_t ncols) {
+  float north_gradient =
+      (u[col * nrows + row] - u[col * nrows + row - 1]) / cellsize;
+  float south_gradient =
+      (u[col * nrows + row + 1] - u[col * nrows + row]) / cellsize;
+
+  float ns_gradient =
+      std::fmaxf(0.0f, std::fmaxf(north_gradient, -south_gradient));
+
+  float west_gradient =
+      (u[col * nrows + row] - u[(col - 1) * nrows + row]) / cellsize;
+  float east_gradient =
+      (u[(col + 1) * nrows + row] - u[col * nrows + row]) / cellsize;
+
+  float ew_gradient =
+      std::fmaxf(0.0f, std::fmaxf(west_gradient, -east_gradient));
+  float g = sqrtf(ns_gradient * ns_gradient + ew_gradient * ew_gradient);
+
+  return g;
+}
+
 int32_t random_dem_test(ptrdiff_t nrows, ptrdiff_t ncols, uint32_t seed) {
   float *dem = new float[nrows * ncols];
-  float *excess = new float[nrows * ncols];
+  float *fmm_excess = new float[nrows * ncols];
+  float *fsm_excess = new float[nrows * ncols];
   ptrdiff_t *heap = new ptrdiff_t[nrows * ncols];
   ptrdiff_t *back = new ptrdiff_t[nrows * ncols];
   float *threshold = new float[nrows * ncols];
@@ -42,48 +65,52 @@ int32_t random_dem_test(ptrdiff_t nrows, ptrdiff_t ncols, uint32_t seed) {
     for (ptrdiff_t row = 0; row < nrows; row++) {
       ptrdiff_t idx = col * nrows + row;
       dem[idx] = 100.0f * pcg4d(row, col, seed, 0);
-      threshold[idx] = 1.0f / sqrtf(3.0);  // 30 degree threshold
+      fsm_excess[idx] = dem[idx];
+      fmm_excess[idx] = dem[idx];
+      threshold[idx] = 1.0f / sqrtf(3.0f);
+          
     }
   }
   float cellsize = 30.0;
-  excesstopography(excess, heap, back, dem, threshold, cellsize, nrows, ncols);
+  fsm_excesstopography(fsm_excess, dem, threshold, cellsize, nrows, ncols);
+  fmm_excesstopography(fmm_excess, heap, back, dem, threshold, cellsize, nrows,
+                       ncols);
 
   for (ptrdiff_t col = 0; col < ncols; col++) {
     for (ptrdiff_t row = 0; row < nrows; row++) {
       ptrdiff_t idx = col * nrows + row;
 
-      if (excess[idx] > dem[idx]) {
+      if (fmm_excess[idx] > dem[idx]) {
         std::cout << "Seed (" << seed << "): Pixel (" << row << ", " << col
                   << ") excess topography is greater than DEM" << std::endl;
         return -1;
       };
 
+      if (fsm_excess[idx] > dem[idx]) {
+        std::cout << "Seed (" << seed << "): Pixel (" << row << ", " << col
+                  << ") FSM excess topography is greater than DEM" << std::endl;
+        return -1;
+      }
+
       // Test the upwind gradient
       if (col > 0 && col < ncols - 1 && row > 0 && row < nrows - 1) {
-        float north_gradient =
-            (excess[col * nrows + row] - excess[col * nrows + row - 1]) /
-            cellsize;
-        float south_gradient =
-            (excess[col * nrows + row - 1] - excess[col * nrows + row]) /
-            cellsize;
+        float g = upwind_gradient(fmm_excess, row, col, cellsize, nrows, ncols);
 
-        float ns_gradient =
-            std::fmaxf(0.0f, std::fmaxf(north_gradient, -south_gradient));
-
-        float west_gradient =
-            (excess[col * nrows + row] - excess[(col - 1) * nrows + row]) /
-            cellsize;
-        float east_gradient =
-            (excess[(col + 1) * nrows + row] - excess[col * nrows + row]) /
-            cellsize;
-
-        float ew_gradient =
-            std::fmaxf(0.0f, std::fmaxf(west_gradient, -east_gradient));
-        float g = ns_gradient * ns_gradient + ew_gradient * ew_gradient;
-
-        if (g > 1.0f / threshold[col * nrows + row]) {
+        if ((g - threshold[col*nrows + row]) > 1e-4) {
           std::cout << "Seed (" << seed << "): Pixel (" << row << ", " << col
                     << ") discrete gradient " << g
+                    << " is greater than threshold" << std::endl;
+          return -1;
+        };
+      }
+
+      // Test the FSM upwind gradient
+      if (col > 0 && col < ncols - 1 && row > 0 && row < nrows - 1) {
+        float g = upwind_gradient(fsm_excess, row, col, cellsize, nrows, ncols);
+
+        if ((g - threshold[col*nrows + row]) > 1e-4) {
+          std::cout << "Seed (" << seed << "): Pixel (" << row << ", " << col
+                    << ") FSM discrete gradient " << g
                     << " is greater than threshold" << std::endl;
           return -1;
         };
