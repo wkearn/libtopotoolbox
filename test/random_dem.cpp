f8860104d01cc2c5ab156652dd907e3a6ad6750f
William Kearney <william.kearney@uni-potsdam.de>
2024-07-08 08:17:12 +0000

    Refactor tests in random_dem.cpp (#66)
    This commit refactors test/random_dem.cpp to make the tests more clear. 
    Each test is extracted to its own function, and the random_dem_test 
    function sets up the data, calls the appropriate libtopotoolbox 
    functions, and then calls the appropriate test functions.
    
    test/gwdt.cpp, which largely duplicated test/random_dem.cpp, is now 
    merged with test/random_dem.cpp. Its tests are put into a function 
    test_gwdt, which is run with the others. This improves the test speed,
    because the flow routing functions like fillsinks only need to be run once.
    
    Undefining the NDEBUG flag in `random_dem.cpp` allows us to use 
    the `assert` macro in our tests in Release mode. Otherwise the asserts 
    are no-ops, and the tests are useless. Since the flag only applies to 
    the `random_dem` tests, it should not affect any asserts in the compiled
    library.
    
    Eventually we may want to use a more complete testing framework with
    proper testing macros.
    
    Signed-off-by: William Kearney <william.kearney@uni-potsdam.de>

--- test/random_dem.cpp
+++ test/random_dem.cpp
@@ -1,3 +1,5 @@
+#undef NDEBUG
+#include <cassert>
 #include <cmath>
 #include <cstddef>
 #include <cstdint>
@@ -6,6 +8,279 @@
 extern "C" {
 #include "topotoolbox.h"
 #include "utils.h"
+}
+
+#define SQRT2f 1.41421356237309504880f
+
+/*
+  Each pixel of the filled DEM should be greater than or equal to
+  the corresponding pixel in the original DEM.
+ */
+int32_t test_fillsinks_ge(float *original_dem, float *filled_dem,
+                          ptrdiff_t nrows, ptrdiff_t ncols) {
+  for (ptrdiff_t j = 0; j < ncols; j++) {
+    for (ptrdiff_t i = 0; i < nrows; i++) {
+      assert(filled_dem[i + nrows * j] >= original_dem[i + nrows * j]);
+    }
+  }
+  return 0;
+}
+
+/*
+  No pixel in the filled DEM should be completely surrounded by pixels higher
+  than it.
+ */
+int32_t test_fillsinks_filled(float *filled_dem, ptrdiff_t nrows,
+                              ptrdiff_t ncols) {
+  ptrdiff_t col_offset[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+  ptrdiff_t row_offset[8] = {1, 0, -1, -1, -1, 0, 1, 1};
+
+  for (ptrdiff_t j = 0; j < ncols; j++) {
+    for (ptrdiff_t i = 0; i < nrows; i++) {
+      float z = filled_dem[i + nrows * j];
+      ptrdiff_t up_neighbor_count = 0;
+      for (int32_t neighbor = 0; neighbor < 8; neighbor++) {
+        ptrdiff_t neighbor_i = i + row_offset[neighbor];
+        ptrdiff_t neighbor_j = j + col_offset[neighbor];
+
+        if (neighbor_i < 0 || neighbor_i >= nrows || neighbor_j < 0 ||
+            neighbor_j >= ncols) {
+          continue;
+        }
+
+        if (filled_dem[neighbor_i + nrows * neighbor_j] > z) {
+          up_neighbor_count++;
+        }
+      }
+      assert(up_neighbor_count != 8);
+    }
+  }
+  return 0;
+}
+
+/*
+  Every pixel not on the boundary with no lower neighbors and fewer than
+  8 higher neighbors should be labeled a flat. Likewise, every flat
+  should have no lower neighbors and fewer than 8 higher neighbors.
+ */
+int32_t test_identifyflats_flats(int32_t *flats, float *dem, ptrdiff_t nrows,
+                                 ptrdiff_t ncols) {
+  ptrdiff_t col_offset[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+  ptrdiff_t row_offset[8] = {1, 0, -1, -1, -1, 0, 1, 1};
+
+  for (ptrdiff_t j = 0; j < ncols; j++) {
+    for (ptrdiff_t i = 0; i < nrows; i++) {
+      float z = dem[i + nrows * j];
+      int32_t flat = flats[i + nrows * j];
+
+      int32_t up_neighbor_count = 0;
+      int32_t down_neighbor_count = 0;
+      for (int32_t neighbor = 0; neighbor < 8; neighbor++) {
+        ptrdiff_t neighbor_i = i + row_offset[neighbor];
+        ptrdiff_t neighbor_j = j + col_offset[neighbor];
+
+        if (neighbor_i < 0 || neighbor_i >= nrows || neighbor_j < 0 ||
+            neighbor_j >= ncols) {
+          continue;
+        }
+
+        float neighbor_height = dem[neighbor_i + nrows * neighbor_j];
+
+        if (neighbor_height > z) {
+          up_neighbor_count++;
+        } else if (neighbor_height < z) {
+          down_neighbor_count++;
+        }
+      }
+      int32_t current_pixel_on_boundary =
+          i == 0 || i == nrows - 1 || j == 0 || j == ncols - 1;
+
+      assert((!current_pixel_on_boundary && (down_neighbor_count == 0) &&
+              (up_neighbor_count < 8)) == ((flat & 1) == 1));
+    }
+  }
+
+  return 0;
+}
+
+/*
+  Every pixel that has a lower neighbor, borders a flat, and has the
+  same elevation as a flat that it touches should be labeled a sill.
+*/
+int32_t test_identifyflats_sills(int32_t *flats, float *dem, ptrdiff_t nrows,
+                                 ptrdiff_t ncols) {
+  ptrdiff_t col_offset[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+  ptrdiff_t row_offset[8] = {1, 0, -1, -1, -1, 0, 1, 1};
+
+  for (ptrdiff_t j = 0; j < ncols; j++) {
+    for (ptrdiff_t i = 0; i < nrows; i++) {
+      float z = dem[i + j * nrows];
+      int32_t flat = flats[i + j * nrows];
+
+      int32_t down_neighbor_count = 0;
+      int32_t equal_neighbor_flats = 0;
+
+      for (int32_t neighbor = 0; neighbor < 8; neighbor++) {
+        ptrdiff_t neighbor_i = i + row_offset[neighbor];
+        ptrdiff_t neighbor_j = j + col_offset[neighbor];
+
+        if (neighbor_i < 0 || neighbor_i >= nrows || neighbor_j < 0 ||
+            neighbor_j >= ncols) {
+          // Count boundary pixels as down neighbors so sills can
+          // drain off the map.
+          down_neighbor_count++;
+          continue;
+        }
+
+        float neighbor_height = dem[neighbor_i + nrows * neighbor_j];
+        int32_t neighbor_flat = flats[neighbor_i + nrows * neighbor_j];
+
+        if (neighbor_height < z) {
+          down_neighbor_count++;
+        }
+
+        if ((neighbor_flat & 1) && (neighbor_height == z)) {
+          equal_neighbor_flats++;
+        }
+      }
+      assert(((flat & 2) > 0) ==
+             ((down_neighbor_count > 0) && (equal_neighbor_flats > 0)));
+    }
+  }
+
+  return 0;
+}
+
+/*
+  Every pixel that is a flat and borders a sill of the same
+  height should be labeled a presill
+*/
+int32_t test_identifyflats_presills(int32_t *flats, float *dem, ptrdiff_t nrows,
+                                    ptrdiff_t ncols) {
+  ptrdiff_t col_offset[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+  ptrdiff_t row_offset[8] = {1, 0, -1, -1, -1, 0, 1, 1};
+
+  for (ptrdiff_t j = 0; j < ncols; j++) {
+    for (ptrdiff_t i = 0; i < nrows; i++) {
+      float z = dem[i + j * nrows];
+      int32_t flat = flats[i + j * nrows];
+
+      int32_t equal_neighbor_sills = 0;
+
+      for (int32_t neighbor = 0; neighbor < 8; neighbor++) {
+        ptrdiff_t neighbor_i = i + row_offset[neighbor];
+        ptrdiff_t neighbor_j = j + col_offset[neighbor];
+
+        if (neighbor_i < 0 || neighbor_i >= nrows || neighbor_j < 0 ||
+            neighbor_j >= ncols) {
+          continue;
+        }
+
+        float neighbor_height = dem[neighbor_i + nrows * neighbor_j];
+        int32_t neighbor_flat = flats[neighbor_i + nrows * neighbor_j];
+
+        if (((neighbor_flat & 2) > 0) && (neighbor_height == z)) {
+          equal_neighbor_sills++;
+        }
+      }
+      assert(((flat & 1) > 0 && (equal_neighbor_sills > 0)) ==
+             ((flat & 4) > 0));
+    }
+  }
+
+  return 0;
+}
+
+/*
+  Costs should be zero on nonflats and positive on flats.
+ */
+int32_t test_gwdt_costs(float *costs, int32_t *flats, ptrdiff_t nrows,
+                        ptrdiff_t ncols) {
+  for (ptrdiff_t j = 0; j < ncols; j++) {
+    for (ptrdiff_t i = 0; i < nrows; i++) {
+      float cost = costs[i + nrows * j];
+      int32_t flat = flats[i + nrows * j];
+
+      assert((cost >= 0) && (((flat & 1) > 0) == (cost > 0)));
+    }
+  }
+
+  return 0;
+}
+
+/*
+  Flats neighboring other flats should all have the same nonzero
+  connected component label
+ */
+int32_t test_gwdt_conncomps(ptrdiff_t *conncomps, int32_t *flats,
+                            ptrdiff_t nrows, ptrdiff_t ncols) {
+  ptrdiff_t col_offset[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+  ptrdiff_t row_offset[8] = {1, 0, -1, -1, -1, 0, 1, 1};
+
+  for (ptrdiff_t j = 0; j < ncols; j++) {
+    for (ptrdiff_t i = 0; i < nrows; i++) {
+      ptrdiff_t label = conncomps[i + nrows * j];
+      int32_t flat = flats[i + nrows * j];
+
+      if (!(flat & 1)) {
+        continue;
+      }
+
+      for (int32_t neighbor = 0; neighbor < 8; neighbor++) {
+        ptrdiff_t neighbor_i = i + row_offset[neighbor];
+        ptrdiff_t neighbor_j = j + col_offset[neighbor];
+
+        if (neighbor_i < 0 || neighbor_i >= nrows || neighbor_j < 0 ||
+            neighbor_j >= ncols) {
+          continue;
+        }
+
+        int32_t neighbor_flat = flats[neighbor_i + nrows * neighbor_j];
+        ptrdiff_t neighbor_label = conncomps[neighbor_i + nrows * neighbor_j];
+        if (neighbor_flat & 1) {
+          assert(neighbor_label == label);
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+/*
+  Gray-weighted distances should be
+
+  1. non-negative for all pixels
+  2. 1 for presills
+  3. >1 for other flats
+  4. equal to the distance to the parent pixel plus the geodesic distance
+  between the parent and the current pixel.
+ */
+int32_t test_gwdt(float *dist, ptrdiff_t *prev, float *costs, int32_t *flats,
+                  ptrdiff_t nrows, ptrdiff_t ncols) {
+  for (ptrdiff_t j = 0; j < ncols; j++) {
+    for (ptrdiff_t i = 0; i < nrows; i++) {
+      float d = dist[i + j * nrows];
+      int32_t flat = flats[i + j * nrows];
+
+      assert(d >= 0);
+
+      if (flat & 4) {
+        assert(d == 1.0);
+      } else if (flat & 1) {
+        assert(d > 1.0);
+
+        ptrdiff_t parent = prev[i + j * nrows];
+        ptrdiff_t parent_j = parent / nrows;
+        ptrdiff_t parent_i = parent % nrows;
+        float chamfer = (parent_j != j) && (parent_i != i) ? SQRT2f : 1.0f;
+        float proposed_dist =
+            dist[parent] + chamfer * (costs[i + j * nrows] + costs[parent]) / 2;
+        assert(proposed_dist == d);
+      }
+    }
+  }
+
+  return 0;
 }
 
 int32_t random_dem_test(ptrdiff_t nrows, ptrdiff_t ncols, uint32_t seed) {
@@ -24,6 +299,12 @@ int32_t random_dem_test(ptrdiff_t nrows, ptrdiff_t ncols, uint32_t seed) {
   ptrdiff_t *conncomps = new ptrdiff_t[nrows * ncols];
   float *costs = new float[nrows * ncols];
 
+  // Outputs and intermediate needs for gwdt
+  float *dist = new float[nrows * ncols];
+  ptrdiff_t *heap = new ptrdiff_t[nrows * ncols];
+  ptrdiff_t *back = new ptrdiff_t[nrows * ncols];
+  ptrdiff_t *prev = new ptrdiff_t[nrows * ncols];
+
   for (uint32_t col = 0; col < ncols; col++) {
     for (uint32_t row = 0; row < nrows; row++) {
       dem[col * nrows + row] = 100.0f * pcg4d(row, col, seed, 1);
@@ -32,208 +313,33 @@ int32_t random_dem_test(ptrdiff_t nrows, ptrdiff_t ncols, uint32_t seed) {
 
   // Run flow routing algorithms
   fillsinks(filled_dem, dem, nrows, ncols);
-  ptrdiff_t count_flats = identifyflats(flats, filled_dem, nrows, ncols);
+
+  test_fillsinks_ge(dem, filled_dem, nrows, ncols);
+  test_fillsinks_filled(filled_dem, nrows, ncols);
+
+  identifyflats(flats, filled_dem, nrows, ncols);
+
+  test_identifyflats_flats(flats, filled_dem, nrows, ncols);
+  test_identifyflats_sills(flats, filled_dem, nrows, ncols);
+  test_identifyflats_presills(flats, filled_dem, nrows, ncols);
+
   gwdt_computecosts(costs, conncomps, flats, dem, filled_dem, nrows, ncols);
 
-  // Number of flats identified in the test
-  ptrdiff_t test_count_flats = 0;
+  test_gwdt_costs(costs, flats, nrows, ncols);
+  test_gwdt_conncomps(conncomps, flats, nrows, ncols);
 
-  // Test properties of filled DEM and identified flats
-  ptrdiff_t col_offset[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
-  ptrdiff_t row_offset[8] = {1, 0, -1, -1, -1, 0, 1, 1};
-
-  for (ptrdiff_t col = 0; col < ncols; col++) {
-    for (ptrdiff_t row = 0; row < nrows; row++) {
-      float z = filled_dem[col * nrows + row];
-      int32_t flat = flats[col * nrows + row];
-      float cost = costs[col * nrows + row];
-      ptrdiff_t label = conncomps[col * nrows + row];
-
-      int32_t current_pixel_on_border =
-          row == 0 || row == nrows - 1 || col == 0 || col == ncols - 1;
-
-      // Each pixel of the filled raster should be >= the DEM
-      if (z < dem[col * nrows + row]) {
-        std::cout << "Pixel (" << row << ", " << col << ") is below the DEM"
-                  << std::endl;
-        std::cout << "Value: " << z << std::endl;
-        std::cout << "DEM: " << dem[col * nrows + row] << std::endl;
-        return -1;
-      }
-
-      // Test cost computation
-      if (flat & 1) {
-        if (cost <= 0) {
-          std::cout << "The cost at pixel (" << row << ", " << col
-                    << ") is nonpositive" << std::endl;
-          return -1;
-        }
-
-        if (label == 0) {
-          std::cout << "Pixel (" << row << ", " << col
-                    << ") is a flat but has a connected component label of 0"
-                    << std::endl;
-          return -1;
-        }
-      }
-
-      if (!(flat & 1)) {
-        if (cost != 0) {
-          std::cout << "The cost " << cost << " at nonflat pixel (" << row
-                    << ", " << col << ") is nonzero" << std::endl;
-          return -1;
-        }
-      }
-
-      // Number of neighbors lower than the current pixel
-      int32_t down_neighbor_count = 0;
-
-      // Number of neighbors higher than the current pixel
-      int32_t up_neighbor_count = 0;
-
-      // Number of neighbors that are flats
-      int32_t neighboring_flats = 0;
-
-      // Number of neighboring flats that have the same elevation as
-      // the current pixel
-      int32_t equal_neighboring_flats = 0;
-
-      // Number of neighboring sills that have the same elevation as
-      // the current pixel
-      int32_t equal_neighboring_sills = 0;
-
-      for (ptrdiff_t neighbor = 0; neighbor < 8; neighbor++) {
-        ptrdiff_t neighbor_row = row + row_offset[neighbor];
-        ptrdiff_t neighbor_col = col + col_offset[neighbor];
-
-        if (neighbor_row < 0 || neighbor_row >= nrows || neighbor_col < 0 ||
-            neighbor_col >= ncols) {
-          continue;
-        }
-
-        float neighbor_height = filled_dem[neighbor_col * nrows + neighbor_row];
-        int32_t neighboring_flat = flats[neighbor_col * nrows + neighbor_row];
-        ptrdiff_t neighbor_label =
-            conncomps[neighbor_col * nrows + neighbor_row];
-
-        if (z < neighbor_height) {
-          up_neighbor_count++;
-        }
-
-        if (z > neighbor_height) {
-          down_neighbor_count++;
-        }
-
-        if (neighboring_flat & 1) {
-          neighboring_flats++;
-
-          if (z == neighbor_height) {
-            equal_neighboring_flats++;
-          }
-
-          // Flats neighboring other flats should all have the same
-          // connected component label
-          if ((flat & 1) && (neighbor_label != label)) {
-            std::cout << "Pixel (" << row << ", " << col
-                      << ") is a flat bordering another flat with a different "
-                         "connected component label"
-                      << std::endl;
-            std::cout << "Label: " << label << std::endl;
-            std::cout << "Neighbor label: " << neighbor_label << std::endl;
-            return -1;
-          }
-        }
-
-        if (neighboring_flat & 2) {
-          if (z == neighbor_height) {
-            equal_neighboring_sills++;
-          }
-        }
-      }
-
-      // No pixel of the filled raster should be surrounded by
-      // neighbors that are higher than it
-      if (up_neighbor_count == 8) {
-        std::cout << "Pixel (" << row << ", " << col << ") is a sink"
-                  << std::endl;
-        return -1;
-      }
-
-      if (!current_pixel_on_border && up_neighbor_count < 8 &&
-          down_neighbor_count == 0) {
-        // This pixel is a flat
-        test_count_flats++;
-      }
-
-      // Every pixel with no lower neighbors and fewer than 8
-      // up_neighbors should be labeled a flat
-      if (!current_pixel_on_border && up_neighbor_count < 8 &&
-          down_neighbor_count == 0 && !(flat & 1)) {
-        std::cout << "Pixel (" << row << ", " << col
-                  << ") is has no lower neighbors but is not labeled a flat"
-                  << std::endl;
-        return -1;
-      }
-
-      // Every pixel that has a lower neighbor, borders a flat, and
-      // has the same elevation as a flat that it touches should be
-      // labeled a sill.
-      if (equal_neighboring_flats > 0 && down_neighbor_count > 0 &&
-          !(flat & 2)) {
-        std::cout << "Pixel (" << row << ", " << col
-                  << ") neighbors a flat and has lower neighbors but is not "
-                     "labeled a sill"
-                  << std::endl;
-        return -1;
-      }
-
-      // Every pixel that is a flat and borders a sill of the same
-      // height should be labeled a presill
-      if ((flat & 1) && (equal_neighboring_sills > 0) && !(flat & 4)) {
-        std::cout
-            << "Pixel (" << row << ", " << col
-            << ") is a flat that neighbors a sill but is not labeled a presill"
-            << std::endl;
-        return -1;
-      }
-
-      // No flat should border a lower pixel
-      if ((flat & 1) && (down_neighbor_count > 0)) {
-        std::cout << "Pixel (" << row << ", " << col
-                  << ") is a flat but has a lower neighbor" << std::endl;
-        return -1;
-      }
-
-      // Every sill pixel should have at least one neighbor lower than it,
-      // unless it is on a border
-      if (!current_pixel_on_border && (flat & 2) &&
-          (down_neighbor_count == 0)) {
-        std::cout << "Pixel (" << row << ", " << col
-                  << ") is a sill but has no lower neighbor" << std::endl;
-        return -1;
-      }
-
-      // Every sill pixel should border a flat
-      if ((flat & 2) && (neighboring_flats == 0)) {
-        std::cout << "Pixel (" << row << ", " << col
-                  << ") is a sill but does not border a flat" << std::endl;
-        return -1;
-      }
-    }
-  }
-
-  if (test_count_flats != count_flats) {
-    std::cout << "Number of flats identified in the test is not equal to the "
-                 "number of flats computed by identifyflats"
-              << std::endl;
-    return -1;
-  }
+  gwdt(dist, prev, costs, flats, heap, back, nrows, ncols);
+  test_gwdt(dist, prev, costs, flats, nrows, ncols);
 
   delete[] dem;
   delete[] filled_dem;
   delete[] flats;
   delete[] conncomps;
   delete[] costs;
+  delete[] dist;
+  delete[] heap;
+  delete[] back;
+  delete[] prev;
 
   return 0;
 }
