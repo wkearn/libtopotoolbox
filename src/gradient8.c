3f4a151a93475cbc4b828b8643a9ff152ac4be74
Teschl <69400012+Teschl@users.noreply.github.com>
2024-08-27 13:31:49 +0200

    Remove uncessesray parallel section

--- src/gradient8.c
+++ src/gradient8.c
@@ -34,57 +34,55 @@ void gradient8(float *output, float *dem, float cellsize, char unit, int use_mp,
                ptrdiff_t dims[2]) {
   ptrdiff_t i_offset[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
   ptrdiff_t j_offset[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
-#pragma omp parallel
-  {
+
 #pragma omp parallel for if (use_mp)
-    for (ptrdiff_t j = 0; j < dims[1]; j++) {
-      for (ptrdiff_t i = 0; i < dims[0]; i++) {
-        float max_gradient = 0;
+  for (ptrdiff_t j = 0; j < dims[1]; j++) {
+    for (ptrdiff_t i = 0; i < dims[0]; i++) {
+      float max_gradient = 0;
 
-        for (int k = 0; k < 8; k++) {
-          ptrdiff_t neighbour_i = i + i_offset[k];
-          ptrdiff_t neighbour_j = j + j_offset[k];
+      for (int k = 0; k < 8; k++) {
+        ptrdiff_t neighbour_i = i + i_offset[k];
+        ptrdiff_t neighbour_j = j + j_offset[k];
 
-          // Check if cells in bounds
-          if (neighbour_i >= 0 && neighbour_i < dims[0] && neighbour_j >= 0 &&
-              neighbour_j < dims[1]) {
-            float horizontal_dist;
-            float vertical_dist;
-            float local_gradient;
+        // Check if cells in bounds
+        if (neighbour_i >= 0 && neighbour_i < dims[0] && neighbour_j >= 0 &&
+            neighbour_j < dims[1]) {
+          float horizontal_dist;
+          float vertical_dist;
+          float local_gradient;
 
-            if (neighbour_i != i && neighbour_j != j) {
-              horizontal_dist = SQRT2 * cellsize;
-            } else {
-              horizontal_dist = cellsize;
-            }
-            vertical_dist = fabsf(dem[neighbour_j * dims[0] + neighbour_i] -
-                                  dem[j * dims[0] + i]);
+          if (neighbour_i != i && neighbour_j != j) {
+            horizontal_dist = SQRT2 * cellsize;
+          } else {
+            horizontal_dist = cellsize;
+          }
+          vertical_dist = fabsf(dem[neighbour_j * dims[0] + neighbour_i] -
+                                dem[j * dims[0] + i]);
 
-            local_gradient = vertical_dist / horizontal_dist;
-            ;
-            if (local_gradient > max_gradient) {
-              max_gradient = local_gradient;
-            }
+          local_gradient = vertical_dist / horizontal_dist;
+          ;
+          if (local_gradient > max_gradient) {
+            max_gradient = local_gradient;
           }
         }
-        // convert results to correct format and save them
-        switch (unit) {
-          case 'r':  // radian
-            output[j * dims[0] + i] = atanf(max_gradient);
-            break;
-          case 'd':  // degree
-            output[j * dims[0] + i] = atanf(max_gradient) * (180.0f / PI);
-            break;
-          case 's':  // sine
-            output[j * dims[0] + i] = sinf(atanf(max_gradient));
-            break;
-          case 'p':  // percent
-            output[j * dims[0] + i] = max_gradient * 100.0f;
-            break;
-          default:  // tangent (default)
-            output[j * dims[0] + i] = max_gradient;
-            break;
-        }
+      }
+      // convert results to correct format and save them
+      switch (unit) {
+        case 'r':  // radian
+          output[j * dims[0] + i] = atanf(max_gradient);
+          break;
+        case 'd':  // degree
+          output[j * dims[0] + i] = atanf(max_gradient) * (180.0f / PI);
+          break;
+        case 's':  // sine
+          output[j * dims[0] + i] = sinf(atanf(max_gradient));
+          break;
+        case 'p':  // percent
+          output[j * dims[0] + i] = max_gradient * 100.0f;
+          break;
+        default:  // tangent (default)
+          output[j * dims[0] + i] = max_gradient;
+          break;
       }
     }
   }
