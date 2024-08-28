d14a8150e320bac42e1bbb05ed2c9f521e355e1f
Teschl <69400012+Teschl@users.noreply.github.com>
2024-08-28 13:09:34 +0200

    Pedeclare parallel variables

--- src/gradient8.c
+++ src/gradient8.c
@@ -35,9 +35,10 @@ void gradient8(float *output, float *dem, float cellsize, char unit, int use_mp,
   ptrdiff_t i_offset[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
   ptrdiff_t j_offset[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
+  ptrdiff_t j, i;
 #pragma omp parallel for if (use_mp)
-  for (ptrdiff_t j = 0; j < dims[1]; j++) {
-    for (ptrdiff_t i = 0; i < dims[0]; i++) {
+  for (j = 0; j < dims[1]; j++) {
+    for (i = 0; i < dims[0]; i++) {
       float max_gradient = 0;
 
       for (int k = 0; k < 8; k++) {
