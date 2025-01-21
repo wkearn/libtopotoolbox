3af178eb15d02703386518728e2c7ba1a18bdc63
William Kearney <william.kearney@uni-potsdam.de>
2025-01-21 10:07:53 +0100

    Assign drainage basins only to valid edges
    The placeholder edges from src -> -1 denote sinks, outlets, and
    invalid pixels (i.e. NaNs). By initializing drainage basins when tgt <
    0, we create drainage basins at the sinks and outlets but also at the
    invalid pixels. This commit changes this behavior to create a new
    basin only upon reaching a valid edge src->tgt where tgt does not yet
    belong to a drainage basin.

--- src/drainagebasins.c
+++ src/drainagebasins.c
@@ -13,18 +13,24 @@ void drainagebasins(ptrdiff_t *basins, ptrdiff_t *source, ptrdiff_t *target,
   }
 
   ptrdiff_t basin_count = 1;  // Start the basin labels at one.
-  for (ptrdiff_t j = dims[1] - 1; j >= 0; j--) {
-    for (ptrdiff_t i = dims[0] - 1; i >= 0; i--) {
-      ptrdiff_t src = source[j * dims[0] + i];
-      ptrdiff_t tgt = target[j * dims[0] + i];
 
-      if (tgt < 0) {
-        // src has no downstream neighbors and is the root of the
-        // drainage basin tree
-        basins[src] = basin_count++;
-      } else {
-        basins[src] = basins[tgt];
-      }
+  ptrdiff_t edge_count = dims[0] * dims[1];
+
+  for (ptrdiff_t e = edge_count - 1; e >= 0; e--) {
+    ptrdiff_t src = source[e];
+    ptrdiff_t tgt = target[e];
+
+    if (tgt < 0) {
+      // src is an outlet
+      //
+      // Don't initialize the basin here: we are refactoring to get
+      // rid of these placeholder edges
+      continue;
     }
+    if (basins[tgt] == 0) {
+      // If tgt does not yet belong to a drainage basin, create a new one
+      basins[tgt] = basin_count++;
+    }
+    basins[src] = basins[tgt];
   }
 }
