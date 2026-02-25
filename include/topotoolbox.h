d5f5e30db7d451a5389d7ca4a813dd57d1153484
William Kearney <william.kearney@uni-potsdam.de>
2026-02-25 12:42:29 +0100

    Add traverse_down_f64_add_mul
    Resolves #208
    
    This is identical to traverse_down_f32_add_mul, but uses double arrays
    instead of float arrays.
    
    This computes flow accumulation/drainage area when `input` is the edge
    weight and `output` is initialized with the initial cell weights using
    double precision can be helpful and necessary when drainage basins are
    large (see #208). Dependencies should choose the appropriate precision
    implementation when computing flow accumulation.
    
    This flow algebra interface will replace `flow_accumulation_edgelist`,
    which does the same thing.  That function and `flow_accumulation` will
    be removed in a future PR. Once this PR is merged, we should update
    dependencies to use the flow algebra interface and ensure that no
    dependencies continue to use `flow_accumulation`.
    
    Signed-off-by: William Kearney <william.kearney@uni-potsdam.de>

--- include/topotoolbox.h
+++ include/topotoolbox.h
@@ -1652,6 +1652,57 @@ void traverse_down_f32_add_mul(float *output, float *input, ptrdiff_t *source,
                                ptrdiff_t *target, ptrdiff_t edge_count);
 
 /**
+   @brief Downstream traversal with multiply-add
+
+   Accumulates the edge weights in the `input` edge attribute list
+   using a multiply-add update:
+
+   for (e = (u,v)) in edges:
+     output[v] = output[v] + output[u] * input[e];
+
+   With input giving the flow fraction of each edge and output
+   initialized to ones, this will compute the flow accumulation.
+
+   @param[out] output The accumulated output
+   @parblock
+   A pointer to a `double` array representing a node attribute list
+
+   This array should have a length equal to the number of nodes in the
+   stream network.  This value must not be less than the largest value
+   in either the `source` or `target` arrays.
+   @endparblock
+
+   @param[in] input The edge weights
+   @parblock
+   A pointer to a `double` array of size `edge_count`
+   @endparblock
+
+   @param[in] source The source node of each edge in the stream
+                     network
+   @parblock
+   A pointer to a `ptrdiff_t` array of size `edge_count`
+
+   The source nodes must be in topological order. The labels must
+   correspond to the 0-based indices of the node-attribute list
+   `output`.
+   @endparblock
+
+   @param[in] target The target nodes of each edge in the stream
+                     network
+   @parblock
+   A pointer to a `ptrdiff_t` array of size `edge_count`
+
+   The labels must correspond to the 0-based indices of the
+   node-attribute list `output`.
+   @endparblock
+
+   @param[in] edge_count The number of edges in the stream network
+ */
+TOPOTOOLBOX_API
+void traverse_down_f64_add_mul(double *output, double *input, ptrdiff_t *source,
+                               ptrdiff_t *target, ptrdiff_t edge_count);
+
+/**
    @brief Compute the in- and outdegrees of each node in a graph
 
    @param[out] indegree The indegree of each node in the stream network
